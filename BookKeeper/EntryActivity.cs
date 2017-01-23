7837351622ee96bdbf334ff999beccbcecb394de
Hrisitijan <hristijan.bocevski@gmail.com>
2017-01-23 21:46:39 +0100

    * .gitignore: added exclude files for xamarin
    * EntryActivity.cs:
    * Entry.axml: created main meny

---
+++ BookKeeper/EntryActivity.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace BookKeeper
+{
+	[Activity(Label = "EntryActivity")]
+	public class EntryActivity : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Create your application here
+		}
+	}
+}
